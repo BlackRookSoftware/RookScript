8117a633ec717165eb22e7345e5b5ac06ad1a643
mtrop <mtropiano@gmail.com>
2019-06-02 17:42:56 -0400

    Update copyright

--- src/main/java/com/blackrook/rookscript/util/HashDequeMap.java
+++ src/main/java/com/blackrook/rookscript/util/HashDequeMap.java
@@ -1,3 +1,9 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Black Rook Software
+ * 
+ * This program and the accompanying materials are made available under 
+ * the terms of the MIT License, which accompanies this distribution.
+ ******************************************************************************/
 package com.blackrook.rookscript.util;
 
 import java.util.Collection;
