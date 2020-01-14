4ea1595ae68187fe42f058635b5d77943b2f4118
mtrop <mtropiano@gmail.com>
2020-01-14 00:54:21 -0500

    Updated copyright year

--- src/main/java/com/blackrook/rookscript/struct/Utils.java
+++ src/main/java/com/blackrook/rookscript/struct/Utils.java
@@ -1,8 +1,9 @@
 /*******************************************************************************
- * Copyright (c) 2019 Black Rook Software
- * 
- * This program and the accompanying materials are made available under 
- * the terms of the MIT License, which accompanies this distribution.
+ * Copyright (c) 2017-2020 Black Rook Software
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
  ******************************************************************************/
 package com.blackrook.rookscript.struct;
 
