ac67ad697aeaceab357762136b02fb7ccddf55ea
mtrop <mtropiano@gmail.com>
2021-02-09 20:06:31 -0500

    Site update

--- js/site.js
+++ js/site.js
@@ -1,26 +1,27 @@
 function github_api_start(data)
 {
-	let repourl = data.repository_url
+	let repourl = null;
+	repourl = data.repository_url
 		.replace('{owner}', REPO_OWNER)
-		.replace('{repo}', REPO_NAME);
-	$INC(repourl + "/releases?callback=display_release");
+		.replace('{repo}', REPO_NAME[0]);
+	$INC(repourl + "/releases?callback=display_release_core");
 }
 
 // ================================================================================
 
 $Q1('body').onload = function()
 {
-	$Q1('#link-github').setAttribute('href', 'https://github.com/'+REPO_OWNER+'/'+REPO_NAME);
-	$Q1('#link-github-release').setAttribute('href', 'https://github.com/'+REPO_OWNER+'/'+REPO_NAME+'/releases');
+	$Q1('#link-github-core').setAttribute('href', 'https://github.com/'+REPO_OWNER+'/'+REPO_NAME[0]);
+	$Q1('#link-github-release-core').setAttribute('href', 'https://github.com/'+REPO_OWNER+'/'+REPO_NAME[0]+'/releases');
 	$INCCALL("https://api.github.com/", github_api_start);
 	$IncludeHTML();
 };
 
 // ================================================================================
 
-function display_release(response)
+function display_release_core(response)
 {
-	display_release_data(response.data[0], $Q1('#releases'), $Q1('#release-version'), $Q1('.site-release-links'));
+	display_release_data(response.data[0], $Q1('#releases-core'), $Q1('#release-version-core'), $Q1('.site-release-links-core'));
 }
 
 function display_release_data(release, release_section_element, release_version_element, release_links_element)
